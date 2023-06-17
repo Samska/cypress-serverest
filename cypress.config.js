d05a5c8145231a53d030257c48fb75dbf68c1544
Samska <samuel.andradetp@live.com>
2023-06-16 21:01:21 -0300

    add configs to use mochawesome report

--- cypress.config.js
+++ cypress.config.js
@@ -1,12 +1,25 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
+  watchForFileChanges: false,
   projectId: 'iumgsn',
+  defaultCommandTimeout: 20000,
+  reporter: "cypress-multi-reporters",
+  reporterOptions: {
+    reporterEnabled: "mochawesome",
+    mochawesomeReporterOptions: {
+      reportDir: "cypress/reports/mocha",
+      quite: true,
+      overwrite: false,
+      html: false,
+      json: true
+    }
+  },
   env: {
     webBaseUrl: 'https://front.serverest.dev',
     apiBaseUrl: 'https://serverest.dev',
   },
   e2e: {
     specPattern: 'cypress/tests'
-  }  
+  }
 });
