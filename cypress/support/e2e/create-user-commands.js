adb22f77a9cdf47dffd98cb580aa99ca4648d7d0
Samska <Samska@users.noreply.github.com>
2023-06-17 15:59:41 +0000

    Update the test report cc5fc3f58eaef44b78582f200f764a1e643b2809

--- cypress/support/e2e/create-user-commands.js
+++
@@ -1,7 +0,0 @@
-Cypress.Commands.add('fillAndSubmitForm', (user) => {
-    cy.get('input[name=nome]').type(user.nome)
-    cy.get('input[name=email]').type(user.email)
-    cy.get('input[name=password]').type(user.password)
-    cy.get('input[name=administrador]').check()
-    cy.get('.btn.btn-primary').click()
-})
